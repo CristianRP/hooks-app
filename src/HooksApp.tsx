37440c6ca413a2728706928cb31b67368d54a8d8
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-03-20 00:47:44 -0600

    init: Initial commit, add typescript configuration, tailwindcss, postcss and HooksApp component

---
+++ src/HooksApp.tsx
@@ -0,0 +1,10 @@
+export const HooksApp = () => {
+  return (
+    <div className="container">
+      <h1 className="text-3xl font-bold underline hover:text-red-200">
+        Hooks App!
+      </h1>
+    </div>
+  )
+}
+
